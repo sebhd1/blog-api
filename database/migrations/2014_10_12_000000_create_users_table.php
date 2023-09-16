4c2797d05a7a373aad6c1c804a87106ab0c23158
Sebastiano Ballotta <sebeballotta@outlook.com>
2023-09-16 15:27:59 +0200

    Removed softDelete from migrations and added profile fields to user's table.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
+    use App\Enums\ProfileStatus;
+    use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
@@ -13,7 +14,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('surname');
+            $table->string('profile_image')->nullable();
             $table->string('email')->unique();
+            $table->text('description')->nullable();
+            $table->string('socials')->nullable();
+            $table->enum('status', ProfileStatus::values());
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
